bd61320152c6460aaf5c9cbc48411b47fdf1ebb5
Renz Ramos <im.renzramos@gmail.com>
2017-09-08 14:46:59 +0800

    Update jquery-auto-height-element.js

--- jquery-auto-height-element.js
+++ jquery-auto-height-element.js
@@ -1,66 +1,23 @@
-/* Real Ramos */
-
-// box auto-height
-  	$(window).resize(function(){
-		initArticleBox();
-		console.log("check");
-	});
+$(window).resize(function(){
+	initArticleBox(".box");
+});
 	
-    $(window).load(function(){
-        initArticleBox();
-    });
+$(window).load(function(){
+	initArticleBox(".box");
+});
+function initArticleBox(selector){
+	var box = $(selector)
+	box.height("auto");
 
-	function initArticleBox(){
-		$(".article-auto-height .content-container").height("auto");
-		$(".article-auto-height .content-container .content").height("auto");
-		$(".article-auto-height .content-container .image").height("auto");
-		$(".article-auto-height h2").height("auto");
-		$(".article-auto-height h3").height("auto");
-		
-		if ($(window).width() > 768){
-			var maxHeight = 0;
-			var article = $(".article-auto-height .content-container").each(function(){
-			
-			    maxHeight = Math.max(maxHeight, jQuery(this).height());
-			    
-			}).height(maxHeight);;
-			
-			var maxHeight = 0;
-			var article = $(".article-auto-height .content-container .content").each(function(){
-			
-			    maxHeight = Math.max(maxHeight, jQuery(this).height());
-			    
-			}).height(maxHeight);;
-			
-// 			var maxHeight = 0;
-// 			var article = $(".article-auto-height .content-container .image").each(function(){
-			
-// 			    maxHeight = Math.max(maxHeight, jQuery(this).height());
-			    
-// 			}).height(maxHeight);;
-			
-			
-			var maxHeight = 0;
-			var article = $(".article-auto-height h2").each(function(){
-			
-			    maxHeight = Math.max(maxHeight, jQuery(this).height());
-			    
-			}).height(maxHeight);;
-			
-			var maxHeight = 0;
-			var article = $(".article-auto-height h3").each(function(){
-			
-			    maxHeight = Math.max(maxHeight, jQuery(this).height());
-			    
-			}).height(maxHeight);;
-			
-			
-			
-		}else{
-			$(".article-auto-height .content-container").height("auto");
-			$(".article-auto-height .content-container .content").height("auto");
-			$(".article-auto-height .content-container .image").height("auto");
-			$(".article-auto-height h2").height("auto");
-			$(".article-auto-height h3").height("auto");
-		}
+	if ($(window).width() > 768){
+		var maxHeight = 0;
+		var article = box.each(function(){
+
+		    maxHeight = Math.max(maxHeight, jQuery(this).height());
+
+		}).height(maxHeight);;
+
+	}else{
+		box.height("auto");
 	}
+}
